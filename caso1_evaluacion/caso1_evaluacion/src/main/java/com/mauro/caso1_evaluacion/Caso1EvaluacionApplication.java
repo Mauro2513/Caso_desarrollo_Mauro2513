f03e7dec4ce264cbec1e4ffc12de8c5f60950abd
Mauro2513 <maucastillo149@gmail.com>
2025-08-28 19:46:23 -0500

    Subo proyecto base caso1_evaluacion

---
+++ caso1_evaluacion/caso1_evaluacion/src/main/java/com/mauro/caso1_evaluacion/Caso1EvaluacionApplication.java
@@ -0,0 +1,13 @@
+package com.mauro.caso1_evaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Caso1EvaluacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Caso1EvaluacionApplication.class, args);
+	}
+
+}
---
+++ caso1_evaluacion/caso1_evaluacion/src/test/java/com/mauro/caso1_evaluacion/Caso1EvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauro.caso1_evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Caso1EvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
